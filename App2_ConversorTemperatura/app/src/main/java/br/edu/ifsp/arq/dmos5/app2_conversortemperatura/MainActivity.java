a48a989a618855bd266e56eca64d986144cc8b9d
bielmelatto <Fwsjx9tp>
2023-03-05 19:09:59 -0300

    teste

---
+++ App2_ConversorTemperatura/app/src/main/java/br/edu/ifsp/arq/dmos5/app2_conversortemperatura/MainActivity.java
@@ -0,0 +1,16 @@
+package br.edu.ifsp.arq.dmos5.app2_conversortemperatura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //teste
+}
